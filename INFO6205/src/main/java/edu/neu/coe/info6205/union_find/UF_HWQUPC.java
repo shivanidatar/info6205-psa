41473017a28c8fa4fee1d050e5fdb6d5bc69fd4c
Shivani Datar <shivanidatar@Shivanis-MacBook-Pro.local>
2023-02-11 14:56:12 -0500

    Assignment 4 Part2 and Part3

--- INFO6205/src/main/java/edu/neu/coe/info6205/union_find/UF_HWQUPC.java
+++ INFO6205/src/main/java/edu/neu/coe/info6205/union_find/UF_HWQUPC.java
@@ -198,4 +198,44 @@ public class UF_HWQUPC implements UF {
         // END 
     }
 
+    public static void main(String[] args) {
+        {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the number of objects");
+            while(sc.hasNext()) {
+
+                int N = sc.nextInt();
+                int count[] = count(N);
+                int total =0;
+                for(int i=0;i<50;i++) {
+                    total+= count[1];
+                }
+                int avg = total/50;
+                System.out.println("No.of objects = " + N + " with an avg pairs of " + avg + " and connections formed "+ count[0]);
+                System.out.println("Enter the number of objects");
+            }
+
+
+        }
+    }
+
+    private static int[] count(int n) {
+        UF_HWQUPC uf = new UF_HWQUPC(n);
+        int connections = 0;
+        int pairs = 0;
+        Random r = new Random();
+        while(uf.components()>1){
+            int p = r.nextInt(n);
+            int q = r.nextInt(n);
+            if(!uf.connected(p,q)){
+                uf.union(p,q);
+                connections +=1;
+            }
+            pairs +=1;
+
+        }
+        int[] connectCount = {connections,pairs};
+        return connectCount;
+    }
 }
